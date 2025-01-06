789e8e3e06735ec822550bb3bd8f8c906c574ea3
manunainala-git57 <manunainala.git@gmail.com>
2025-01-06 18:17:49 +0530

    second part completed - UI design

---
+++ frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Message from './Message'
+
+const Messages = () => {
+  return (
+    <div className='px-4 flex-1 overflow-y-auto h-[500px] custom-scrollbar'>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+        <Message/>
+
+    </div>
+    
+
+  )
+}
+
+export default Messages
