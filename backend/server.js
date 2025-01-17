56efab217247b0d729da4e71d5f7443c45c32379
manunainala-git57 <manunainala.git@gmail.com>
2025-01-17 13:34:45 +0530

    Deployement

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-// import cors from 'cors';
+
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -11,12 +12,14 @@ import {app , server} from './socket/socket.js';
 import connectToMongoDB from './DB/connectToMongoDB.js';
 
 
-const PORT = process.env.PORT || 7000;
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
 app.use(express.json());// a middleware to parse the incoming requests wih JSON payloads (from req.body)
 app.use(cookieParser());//a middleware in order to access the cookies 
-// app.use(cors());
 
 
 
@@ -25,6 +28,12 @@ app.use(cookieParser());//a middleware in order to access the cookies
 app.use('/api/auth' , authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 // app.get('/' , (req,res) =>{
